27439e4044a0cc989273447d07dee30d2161d57c
Ryan Vessell <rvessell@gatech.edu>
2020-09-19 03:08:28 +0000

    Completed exercise 1.0

--- exercise_1_0/exercise_1_0.cpp
+++ exercise_1_0/exercise_1_0.cpp
@@ -2,5 +2,5 @@
 
 int main() {
     // TODO: Make this program print "Hello, world!" to the console.
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 }
